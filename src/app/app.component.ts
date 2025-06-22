6ccfc6f47250b51afbd3a972f72ff4e6b8709ff8
Weebeveloper <aaas.com.com@gmail.com>
2025-06-22 20:08:20 +0300

    build

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
+import anime from 'animejs/lib/anime.es.js';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'babaiov';
+export class AppComponent implements AfterViewInit {
+  splashscreen = true;
+  @ViewChild('soldier') soldier!: ElementRef;
+
+  ngAfterViewInit(): void {
+    setTimeout(() => {}, 8000);
+    anime
+      .timeline()
+      .add({
+        targets: ['.person'],
+        translateX: `-50vw`,
+        duration: 4000,
+        easing: 'easeOutSine',
+      })
+      .add({
+        targets: ['.person'],
+        scaleY: `0.8`,
+        scaleX: '0.8',
+        duration: 2000,
+        begin: () => {
+          this.soldier.nativeElement.style.backgroundImage =
+            'url(../assets/soldier-sit.png)';
+          anime({
+            targets: ['.title'],
+            delay: 400,
+            opacity: 1,
+            easing: 'easeOutExpo',
+            duration: 8000,
+          });
+        },
+      });
+  }
 }
